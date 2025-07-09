fb193acb28bce426ecf0e18d5f445111eac311b4
Tom Morara <morara1920@gmail.com>
2025-07-09 14:14:22 +0300

    Update server.js to serve static frontend and expose products.html

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, 'frontend')));
 
 // Serve uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
